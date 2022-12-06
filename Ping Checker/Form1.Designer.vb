0ebf0ed5a1e97bb1313b6a77bc01149507c9fe66
30044908 <30044908@4T81VN2>
2022-12-06 10:50:55 +0000

    Add project files.

---
+++ Ping Checker/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(12, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 41)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(228, 23)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Enter Address Below"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 15)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Status Label"
+        '
+        'Timer1
+        '
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(165, 82)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Stop"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(107, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 158)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(12, 177)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(41, 15)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label6.Location = New System.Drawing.Point(12, 192)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(40, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Label6"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.ClientSize = New System.Drawing.Size(262, 251)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Ping Checker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Ping Checker/Form1.vb
@@ -0,0 +1,125 @@
+﻿Imports System.CodeDom
+Imports System.Environment
+Imports System.IO
+Imports System.Timers
+
+Public Class Form1
+    Dim SERVER As String
+    Dim counter As Integer
+    Dim UserDesktop As String
+    Dim strFile As String = "C:\TEMP\PingLog_" & DateTime.Today.ToString("dd-MMM-yyyy") & ".txt"
+    Dim sw As StreamWriter
+    REM Dim fs As FileStream = Nothing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.WhiteSmoke
+        Timer1.Interval = 1000
+
+        Button1.Enabled = True
+        Button2.Enabled = False
+
+
+
+        SERVER = ""
+        TextBox1.Text = SERVER
+        Label1.Text = "Enter Ping Address Below"
+        Label2.Text = ""
+        Label3.Text = ""
+        Label4.Text = ""
+        UserDesktop = Environment.GetFolderPath(SpecialFolder.DesktopDirectory)
+        Label5.Text = "Log File Path"
+        Label6.Text = "\Desktop" & "\PingLog_" & DateTime.Today.ToString("dd-MMM-yyyy") & ".txt"
+        strFile = (UserDesktop & "\PingLog_" & DateTime.Today.ToString("dd-MMM-yyyy") & ".txt")
+        CheckorCreateFile()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button1.Enabled = False
+
+
+        If TextBox1.Text = "" Then
+            MsgBox("Duh!! Fill in the box first")
+            Button1.Enabled = True
+            StopAll()
+        Else
+            Button2.Enabled = True
+            Timer1.Start()
+            SERVER = TextBox1.Text
+        End If
+
+
+    End Sub
+
+
+
+
+
+
+
+    Private Sub Timer1_Tick(sender As Object, e As System.EventArgs) Handles Timer1.Tick
+        counter = counter + 1
+        Label3.Text = counter
+        Try
+            If My.Computer.Network.Ping(SERVER) Then
+                Me.BackColor = Color.WhiteSmoke
+                Label2.ForeColor = Color.Green
+                Label2.Text = ("Ping Successful to " & SERVER)
+                Label4.Text = (System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")) & " - Success"
+                AppendLog()
+            Else
+                Label2.Text = ("Ping Failed to " & SERVER)
+
+            End If
+        Catch ex As Exception
+            Label2.ForeColor = Color.Black
+            Me.BackColor = Color.Red
+            Label4.Text = (System.DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss")) & " - Failed"
+            Label2.Text = ("Failed")
+            AppendLog()
+
+        End Try
+    End Sub
+
+
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Button2.Enabled = False
+        Button1.Enabled = True
+        StopAll()
+    End Sub
+
+
+    Private Sub StopAll()
+        Timer1.Stop()
+        Label3.Text = ""
+        Label2.Text = ""
+        Me.BackColor = Color.WhiteSmoke
+        Label2.ForeColor = Color.Black
+        Label4.Text = ""
+        counter = 0
+
+    End Sub
+
+
+
+
+
+
+    Private Sub CheckorCreateFile()
+        If Not System.IO.File.Exists(strFile) Then
+            System.IO.File.Create(strFile).Dispose()
+        End If
+
+    End Sub
+
+    Private Sub AppendLog()
+
+        sw = File.AppendText(strFile)
+        sw.WriteLine(SERVER & " - " & Label4.Text)
+        sw.Close()
+    End Sub
+
+
+
+
+End Class
